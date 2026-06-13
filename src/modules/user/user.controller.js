273c2c984f184e073f2e6f7b7d7d22f252a61a1d
alaaM <alaaMandour2024@gmail.com>
2026-06-13 23:30:11 +0300

    get all users

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as userScervice from "./user.service.js";
-import  authMiddleware  from "../../common/middleware/authentication.js";
+import authMiddleware from "../../common/middleware/authentication.js";
 import authorization from "../../common/middleware/authorization.js";
 import { RoleEnum } from "../../common/enum/user.enum.js";
 import validationMid from "../../common/middleware/validation.js";
@@ -9,62 +9,117 @@ import { multer_host, multer_local } from "../../common/middleware/multer.js";
 import { MimeEnum } from "../../common/enum/multer.enum.js";
 
 const userRouter = Router({
-    caseSensitive:true,
-    strict:true
-})
+  caseSensitive: true,
+  strict: true,
+});
 
 userRouter.post(
-    "/signUp",
-    multer_host({file_type:MimeEnum.images})  //multer middleware before validation because multer pars the data from type form data to json not express.json()
+  "/signUp",
+  multer_host({ file_type: MimeEnum.images }) //multer middleware before validation because multer pars the data from type form data to json not express.json()
     .single("profile_pic"),
-    validationMid({schema:userValidation.signUp_schema}),
-    userScervice.signUp)
-
+  validationMid({ schema: userValidation.signUp_schema }),
+  userScervice.signUp,
+);
 
 // test sinup with multi pictures
 userRouter.post(
-    "/signUp/pics",
-    multer_host({file_type:MimeEnum.images})
-    .array("profile_pic",2),
-    validationMid({schema:userValidation.signUp_schema}),
-    userScervice.signUp_with_multi_pictures)
+  "/signUp/pics",
+  multer_host({ file_type: MimeEnum.images }).array("profile_pic", 2),
+  validationMid({ schema: userValidation.signUp_schema }),
+  userScervice.signUp_with_multi_pictures,
+);
 
 // test sinup with multi fields
 userRouter.post(
-    "/signUp/fields",
-    multer_local({folder_path:"users",file_type:[...MimeEnum.images,...MimeEnum.docs]})
-    .fields([
-    {name:"profile_pic", maxCount: 1},
-    {name:"cover_pic",maxCount: 2},
-]),
-validationMid({schema:userValidation.signUp_schema}),
-userScervice.signUp_with_deffirent_feilds)
+  "/signUp/fields",
+  multer_local({
+    folder_path: "users",
+    file_type: [...MimeEnum.images, ...MimeEnum.docs],
+  }).fields([
+    { name: "profile_pic", maxCount: 1 },
+    { name: "cover_pic", maxCount: 2 },
+  ]),
+  validationMid({ schema: userValidation.signUp_schema }),
+  userScervice.signUp_with_deffirent_feilds,
+);
 
+userRouter.post("/signup/gmail", userScervice.signUpWithGmail);
 
-userRouter.post("/signup/gmail",userScervice.signUpWithGmail)
+userRouter.post(
+  "/signIn",
+  validationMid({ schema: userValidation.signIn_schema }),
+  userScervice.signIn,
+);
 
-userRouter.post("/signIn",validationMid({schema:userValidation.signIn_schema}),userScervice.signIn)
+userRouter.get(
+  "/",
+  authMiddleware,
+  authorization([RoleEnum.user]),
+  userScervice.getMyProfile,
+);
 
-userRouter.get("/",authMiddleware,authorization([RoleEnum.user]),userScervice.getMyProfile)
+userRouter.get(
+  "/:id",
+  authMiddleware,
+  authorization([RoleEnum.user]),
+  userScervice.getProfile,
+);
 
-userRouter.get("/:id",authMiddleware,authorization([RoleEnum.user]),userScervice.getProfile)
+userRouter.post("/refreshToken", userScervice.refreshToken);
 
-userRouter.post("/refreshToken",userScervice.refreshToken)
+userRouter.get(
+  "/shareProfile/:id",
+  validationMid({ schema: userValidation.shareProfile_schema }),
+  userScervice.shareProfile,
+);
 
-userRouter.get("/shareProfile/:id",validationMid({schema:userValidation.shareProfile_schema}),userScervice.shareProfile)
+userRouter.post(
+  "/forgotPassword",
+  validationMid({ schema: userValidation.forgotPassword_schema }),
+  userScervice.forgotPassword,
+);
 
-userRouter.post("/forgotPassword",validationMid({schema:userValidation.forgotPassword_schema}),userScervice.forgotPassword)
+userRouter.patch(
+  "/resetPassword",
+  validationMid({ schema: userValidation.resetPassword_schema }),
+  userScervice.resetPassword,
+);
 
-userRouter.patch("/resetPassword",validationMid({schema:userValidation.resetPassword_schema}),userScervice.resetPassword)
+userRouter.patch(
+  "/updateProfile",
+  validationMid({ schema: userValidation.updateProfile_schema }),
+  authMiddleware,
+  userScervice.updateProfile,
+);
 
-userRouter.patch("/updateProfile",validationMid({schema:userValidation.updateProfile_schema}),authMiddleware,userScervice.updateProfile)
+userRouter.patch(
+  "/updatePassword",
+  validationMid({ schema: userValidation.updatePassword_schema }),
+  authMiddleware,
+  userScervice.updatePassword,
+);
 
-userRouter.patch("/updatePassword",validationMid({schema:userValidation.updatePassword_schema}),authMiddleware,userScervice.updatePassword)
+userRouter.post("/logOut", authMiddleware, userScervice.logOut);
 
-userRouter.post("/logOut",authMiddleware,userScervice.logOut)
+userRouter.post(
+  "/confirm",
+  validationMid({ schema: userValidation.confirmEmial_schema }),
+  userScervice.confirmEmail,
+);
 
-userRouter.post("/confirm",validationMid({schema:userValidation.confirmEmial_schema}),userScervice.confirmEmail)
+userRouter.post(
+  "/resend",
+  validationMid({ schema: userValidation.resendEmial_schema }),
+  userScervice.resendEmail,
+);
 
-userRouter.post("/resend",validationMid({schema:userValidation.resendEmial_schema}),userScervice.resendEmail)
+userRouter.get("/all", authMiddleware, async (req, res) => {
+  const users = await dbService.find({
+    model: userModel,
+    filter: { confirmed: true },
+    select: "_id userName first_name last_name profilePic gender",
+  });
+  return success.success_response({ res, data: users });
+});
 
-export default userRouter
+export default userRouter;
