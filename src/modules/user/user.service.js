4905ad78b608fe389d178f3b97c025c58cb5e8b7
alaaM <alaaMandour2024@gmail.com>
2026-06-14 00:40:59 +0300

    decrept phone in getProfile

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -491,7 +491,7 @@ export const getMyProfile = async (req, res) => {
     return success.success_response({ res, data: user_exist });
   }
 
-  const userData = user_exist;
+  const userData = req.user;
   if (userData.phone) {
     userData.phone = decrypt(userData.phone);
   }
