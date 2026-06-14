ea4824c4a927094b7ba2d6585854232c1ef29f06
alaaM <alaaMandour2024@gmail.com>
2026-06-14 23:40:39 +0300

    cron handelling

--- src/modules/user/user.cron.js
+++ src/modules/user/user.cron.js
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import * as dbService from "../../DB/db.service.js"
 import userModel from '../../DB/models/user.model.js';
 
-cron.schedule('* * * * 0', async () => {
+cron.schedule('0 0 * * 0', async () => {
     const users = await dbService.deleteMany({
         model:userModel,
         filter:{
